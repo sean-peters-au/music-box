d26eb6187ed9e50ec0fb03f17ed438e42bcd259e
Sean Peters <sean.peters.au@gmail.com>
2025-01-15 20:22:16 +1100

    cleanup done

--- README.md
+++ README.md
@@ -1,17 +1,36 @@
 # Music Box
 
-This is 
+This is a simple project to modify a mechnical music box with custom songs. 
 
-https://www.aliexpress.com/item/1005006822293186.html
+This seemed like a fun idea to test out my new 3D printer, and to put a smile on my 4 month old son's face. It worked! Although - probably more efficient to just pull funny faces at him :)
 
+## Instructions
 
+- Buy this music box https://www.aliexpress.com/item/1005006822293186.html
 
-## Getting Started
-
-```
+- Setup your environment
+```sh
 conda create -n music_box python=3.10
 conda activate music_box
 conda install -c conda-forge cadquery trimesh
 pip install git+https://github.com/gumyr/cq_warehouse.git#egg=cq-warehouse
-python wheel.py
 ```
+
+- Print cassette from your mp3
+```sh
+python musicbox.py mp3/twinkle-twinkle-little-star.mp3
+```
+
+- Print the components in `stl/`
+
+- Remove the existing spindle (I managed to just reverse it out with an appropriately sized drill bit).
+<image>
+
+- Remove the existing cassette
+<image>
+
+- Place in new cassette
+<image>
+
+- Thread in the new spindle.
+<image>
--- cassette.py
+++ cassette.py
@@ -1,260 +1,283 @@
+"""
+File: cassette.py
+Generates a 3D cassette wheel for a music box.
+"""
+
 import cadquery as cq
 import math
 
-"""
-Creates a music box cassette wheel with:
-- Base ring with 5mm center hole
-- Main cylinder with spiral pins for the music comb
-- Top assembly with large and small cogs for the winding mechanism
-"""
+class Cassette:
+    """
+    Builds a music box cassette wheel using CadQuery.
+    
+    Public Methods:
+        __init__(): Initializes the Cassette object and builds the geometry.
+        export(filename: str): Exports the cassette as an STL file.
+        simulate(): Simulates the music box by playing the cassette (placeholder).
+    """
 
-# Main cassette dimensions
-cassette_diameter = 13.0
-cassette_radius = cassette_diameter / 2.0
-cassette_total_height = 17.5
-cassette_wall_thickness = 4.0
+    def __init__(self):
+        # Core cassette dimensions
+        self.cassette_diameter = 13.0
+        self.cassette_radius = self.cassette_diameter / 2.0
+        self.cassette_total_height = 17.5
+        self.cassette_wall_thickness = 4.0
 
-# Tine dimensions
-total_tines = 18
-total_tine_width = 16.0
-tine_width = total_tine_width / total_tines
+        # Tine and pin dimensions
+        self.total_tines = 18
+        self.total_tine_width = 16.0
+        self.tine_width = self.total_tine_width / self.total_tines
+        self.num_pins = self.total_tines
+        self.pin_radial_bump = 0.5
+        self.pin_width = self.tine_width / 2
+        self.pin_height = self.tine_width / 2
+        self.pin_vertical_offset = 1.0
 
-# Pin dimensions
-num_pins = total_tines
-pin_radial_bump = 0.5
-pin_width = tine_width / 2
-pin_height = tine_width / 2
-pin_vertical_offset = 1.0
+        # Base ring dimensions
+        self.base_ring_height = 1.8
+        self.base_ring_od = 15.0
+        self.base_ring_id = 5.0
 
-# Base ring dimensions
-base_ring_height = 1.8
-base_ring_od = 15.0
-base_ring_id = 5.0
+        # Top assembly dimensions
+        self.lower_circle_height = 0.5
+        self.lower_circle_diam = 16.0
+        self.big_cog_height = 1.8
+        self.small_cog_height = 1.5
+        self.top_circle_height = 0.7
+        
+        self.big_cog_base_diam = 17.0
+        self.big_cog_teeth_diam = 20.0
+        self.num_teeth_big = 46
+        
+        self.small_cog_base_diam = 5.0
+        self.small_cog_teeth_diam = 8.0
+        self.num_teeth_small = 12
+        
+        self.top_circle_diam = 2.0
 
-# Build base ring
-base_ring = (
-    cq.Workplane("XY")
-    .circle(base_ring_od / 2.0)
-    .extrude(base_ring_height)
-    .cut(
-        cq.Workplane("XY")
-        .circle(base_ring_id / 2.0)
-        .extrude(base_ring_height)
-    )
-)
+        # Musical information
+        self.tine_notes = [
+            "C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5",
+            "E5", "F5", "G5", "A5", "B5", "C6", "D6", "E6", "F6"
+        ]
+        self.cassette_rotation_seconds = 20.0
 
-# Build main cassette body
-outer_cylinder = (
-    cq.Workplane("XY")
-    .workplane(offset=base_ring_height)
-    .circle(cassette_radius)
-    .extrude(cassette_total_height)
-)
+        # Build final cassette geometry
+        self._assembly = self._build_cassette()
 
-inner_cylinder = (
-    cq.Workplane("XY")
-    .workplane(offset=base_ring_height)
-    .circle(cassette_radius - cassette_wall_thickness)
-    .extrude(cassette_total_height)
-)
+    def export(self, filename: str):
+        """
+        Exports the cassette as an STL file with the specified filename.
+        
+        Args:
+            filename (str): Output STL filename.
+        """
+        self._assembly.val().exportStl(filename, tolerance=0.01)
+        print(f"Exported '{filename}'")
 
-cassette_body = outer_cylinder.cut(inner_cylinder)
+    def simulate(self):
+        """
+        Simulates the music box by playing the cassette (placeholder).
+        """
+        print("Simulating cassette... (audio simulation not implemented)")
 
-# Create spiral pins
-pins = cq.Workplane("XY")
-z_min = base_ring_height + pin_vertical_offset
+    def _build_cassette(self) -> cq.Workplane:
+        base_ring = self._make_base_ring()
+        cassette_body = self._make_cassette_body()
+        pins = self._make_pins()
+        wheel_with_pins_and_base = cassette_body.union(pins).union(base_ring)
+        top_assembly = self._make_top_assembly()
+        return wheel_with_pins_and_base.union(top_assembly)
 
-for i in range(num_pins):
-    angle_deg = i * (360.0 / num_pins)
-    angle_rad = math.radians(angle_deg)
-
-    centre_offset = pin_width / 2
-    spacing_offset = tine_width * i
-    z_pin_center = z_min + spacing_offset + centre_offset
-
-    x_center = cassette_radius * math.cos(angle_rad)
-    y_center = cassette_radius * math.sin(angle_rad)
-
-    z_bottom = z_pin_center - (pin_height / 2.0)
-
-    single_pin = (
-        cq.Workplane("XY")
-        .box(
-            length=pin_radial_bump,
-            width=pin_width,
-            height=pin_height,
-            centered=(False, True, False)
+    def _make_base_ring(self) -> cq.Workplane:
+        return (
+            cq.Workplane("XY")
+            .circle(self.base_ring_od / 2.0)
+            .extrude(self.base_ring_height)
+            .cut(
+                cq.Workplane("XY")
+                .circle(self.base_ring_id / 2.0)
+                .extrude(self.base_ring_height)
+            )
         )
-        .rotate((0, 0, 0), (0, 0, 1), angle_deg)
-        .translate((x_center, y_center, z_bottom))
-    )
-    pins = pins.union(single_pin)
 
-wheel_with_pins_and_base = cassette_body.union(pins).union(base_ring)
+    def _make_cassette_body(self) -> cq.Workplane:
+        outer_cylinder = (
+            cq.Workplane("XY")
+            .workplane(offset=self.base_ring_height)
+            .circle(self.cassette_radius)
+            .extrude(self.cassette_total_height)
+        )
+        inner_cylinder = (
+            cq.Workplane("XY")
+            .workplane(offset=self.base_ring_height)
+            .circle(self.cassette_radius - self.cassette_wall_thickness)
+            .extrude(self.cassette_total_height)
+        )
+        return outer_cylinder.cut(inner_cylinder)
 
-# Top assembly dimensions
-lower_circle_height = 0.5
-lower_circle_diam = 16.0
-big_cog_height = 1.8
-small_cog_height = 1.5
-top_circle_height = 0.7
+    def _make_pins(self) -> cq.Workplane:
+        pins = cq.Workplane("XY")
+        z_min = self.base_ring_height + self.pin_vertical_offset
 
-# Calculate z-positions for stacked components
-lower_circle_z_start = base_ring_height + cassette_total_height
-big_cog_z_start = lower_circle_z_start + lower_circle_height
-big_cog_z_top = big_cog_z_start + big_cog_height
-small_cog_z_start = big_cog_z_top
-small_cog_z_top = small_cog_z_start + small_cog_height
-top_circle_z_start = small_cog_z_top
-top_circle_z_top = top_circle_z_start + top_circle_height
+        for i in range(self.num_pins):
+            angle_deg = i * (360.0 / self.num_pins)
+            angle_rad = math.radians(angle_deg)
+            centre_offset = self.pin_width / 2
+            spacing_offset = self.tine_width * i
+            z_pin_center = z_min + spacing_offset + centre_offset
+            x_center = self.cassette_radius * math.cos(angle_rad)
+            y_center = self.cassette_radius * math.sin(angle_rad)
+            z_bottom = z_pin_center - (self.pin_height / 2.0)
 
-# Create lower circle
-lower_circle = (
-    cq.Workplane("XY")
-    .workplane(offset=lower_circle_z_start)
-    .circle(lower_circle_diam / 2.0)
-    .extrude(lower_circle_height)
-)
+            single_pin = (
+                cq.Workplane("XY")
+                .box(
+                    length=self.pin_radial_bump,
+                    width=self.pin_width,
+                    height=self.pin_height,
+                    centered=(False, True, False)
+                )
+                .rotate((0, 0, 0), (0, 0, 1), angle_deg)
+                .translate((x_center, y_center, z_bottom))
+            )
+            pins = pins.union(single_pin)
+        return pins
 
-# Large cog dimensions
-big_cog_base_diam = 17.0
-big_cog_teeth_diam = 20.0
-num_teeth_big = 46
-big_cog_base_radius = big_cog_base_diam / 2.0
-big_cog_teeth_radius = big_cog_teeth_diam / 2.0
-big_cog_radial_thick = big_cog_teeth_radius - big_cog_base_radius
+    def _make_top_assembly(self) -> cq.Workplane:
+        lower_circle_z_start = self.base_ring_height + self.cassette_total_height
+        big_cog_z_start = lower_circle_z_start + self.lower_circle_height
+        big_cog_z_top = big_cog_z_start + self.big_cog_height
+        small_cog_z_start = big_cog_z_top
+        small_cog_z_top = small_cog_z_start + self.small_cog_height
+        top_circle_z_start = small_cog_z_top
 
-# Small cog dimensions
-small_cog_base_diam = 5.0
-small_cog_teeth_diam = 8.0
-num_teeth_small = 12
-small_cog_base_radius = small_cog_base_diam / 2.0
-small_cog_teeth_radius = small_cog_teeth_diam / 2.0
-small_cog_radial_thick = small_cog_teeth_radius - small_cog_base_radius
+        lower_circle = (
+            cq.Workplane("XY")
+            .workplane(offset=lower_circle_z_start)
+            .circle(self.lower_circle_diam / 2.0)
+            .extrude(self.lower_circle_height)
+        )
 
-top_circle_diam = 2.0
+        big_cog = self._make_big_cog(big_cog_z_start)
+        small_cog = self._make_small_cog(small_cog_z_start)
+        
+        top_circle = (
+            cq.Workplane("XY")
+            .workplane(offset=top_circle_z_start)
+            .circle(self.top_circle_diam / 2.0)
+            .extrude(self.top_circle_height)
+        )
 
-# Helper functions to create 3D-tooth solids
-def make_big_cog_tooth(radial_thickness, base_width, tip_width, tooth_height, fillet_3d=0.1):
-    """
-    Makes a trapezoidal "triangular" tooth in 2D, extrudes it, then fillets
-    the vertical edges. Coordinates for the trapezoid:
-        (0, -base_width/2) → (0, base_width/2)
-        → (radial_thickness, tip_width/2) → (radial_thickness, -tip_width/2)
-    """
-    shape_2d = (
-        cq.Workplane("XY")
-        .polyline([
-            (0, -base_width/2.0),
-            (0,  base_width/2.0),
-            (radial_thickness,  tip_width/2.0),
-            (radial_thickness, -tip_width/2.0)
-        ])
-        .close()
-    )
-    tooth_3d = shape_2d.extrude(tooth_height)
-    tooth_3d = tooth_3d.edges("|Z").fillet(fillet_3d)
-    return tooth_3d
+        return lower_circle.union(big_cog).union(small_cog).union(top_circle)
 
-def make_small_cog_tooth(radial_thickness, tooth_width, tooth_height, fillet_3d=0.4):
-    """
-    Makes a rectangular tooth in 2D, extrudes it, then fillets all edges heavily.
-    """
-    shape_2d = (
-        cq.Workplane("XY")
-        .rect(radial_thickness, tooth_width, centered=(False, True))
-    )
-    tooth_3d = shape_2d.extrude(tooth_height)
-    tooth_3d = tooth_3d.edges().fillet(fillet_3d)
-    return tooth_3d
+    def _make_big_cog(self, z_start: float) -> cq.Workplane:
+        big_cog_base_radius = self.big_cog_base_diam / 2.0
+        big_cog_teeth_radius = self.big_cog_teeth_diam / 2.0
+        big_cog_radial_thick = big_cog_teeth_radius - big_cog_base_radius
 
-# Base cylinders for each cog
-big_cog_base = (
-    cq.Workplane("XY")
-    .workplane(offset=big_cog_z_start)
-    .circle(big_cog_base_radius)
-    .extrude(big_cog_height)
-)
+        big_cog_base = (
+            cq.Workplane("XY")
+            .workplane(offset=z_start)
+            .circle(big_cog_base_radius)
+            .extrude(self.big_cog_height)
+        )
 
-small_cog_base = (
-    cq.Workplane("XY")
-    .workplane(offset=small_cog_z_start)
-    .circle(small_cog_base_radius)
-    .extrude(small_cog_height)
-)
+        big_cog_teeth = cq.Workplane("XY")
+        for i in range(self.num_teeth_big):
+            angle_deg = i * (360.0 / self.num_teeth_big)
+            angle_rad = math.radians(angle_deg)
+            tooth_3d = self._make_big_cog_tooth(
+                radial_thickness=big_cog_radial_thick,
+                base_width=1.0,
+                tip_width=0.4,
+                tooth_height=self.big_cog_height,
+                fillet_3d=0.1
+            )
+            tooth_3d = (
+                tooth_3d
+                .rotate((0, 0, 0), (0, 0, 1), angle_deg)
+                .translate((
+                    big_cog_base_radius * math.cos(angle_rad),
+                    big_cog_base_radius * math.sin(angle_rad),
+                    z_start
+                ))
+            )
+            big_cog_teeth = big_cog_teeth.union(tooth_3d)
 
-# Teeth for the large cog (triangular, small fillets)
-big_cog_teeth = cq.Workplane("XY")
+        return big_cog_base.union(big_cog_teeth)
 
-for i in range(num_teeth_big):
-    angle_deg = i * (360.0 / num_teeth_big)
-    angle_rad = math.radians(angle_deg)
+    def _make_small_cog(self, z_start: float) -> cq.Workplane:
+        small_cog_base_radius = self.small_cog_base_diam / 2.0
+        small_cog_teeth_radius = self.small_cog_teeth_diam / 2.0
+        small_cog_radial_thick = small_cog_teeth_radius - small_cog_base_radius
 
-    tooth_3d = make_big_cog_tooth(
-        radial_thickness = big_cog_radial_thick,
-        base_width       = 1.0,
-        tip_width        = 0.4,
-        tooth_height     = big_cog_height,
-        fillet_3d        = 0.1
-    )
+        small_cog_base = (
+            cq.Workplane("XY")
+            .workplane(offset=z_start)
+            .circle(small_cog_base_radius)
+            .extrude(self.small_cog_height)
+        )
 
-    tooth_3d = (
-        tooth_3d
-        .rotate((0,0,0), (0,0,1), angle_deg)
-        .translate((
-            big_cog_base_radius * math.cos(angle_rad),
-            big_cog_base_radius * math.sin(angle_rad),
-            big_cog_z_start
-        ))
-    )
-    big_cog_teeth = big_cog_teeth.union(tooth_3d)
+        small_cog_teeth = cq.Workplane("XY")
+        for i in range(self.num_teeth_small):
+            angle_deg = i * (360.0 / self.num_teeth_small)
+            angle_rad = math.radians(angle_deg)
+            tooth_3d = self._make_small_cog_tooth(
+                radial_thickness=small_cog_radial_thick,
+                tooth_width=1.0,
+                tooth_height=self.small_cog_height,
+                fillet_3d=0.4
+            )
+            tooth_3d = (
+                tooth_3d
+                .rotate((0, 0, 0), (0, 0, 1), angle_deg)
+                .translate((
+                    small_cog_base_radius * math.cos(angle_rad),
+                    small_cog_base_radius * math.sin(angle_rad),
+                    z_start
+                ))
+            )
+            small_cog_teeth = small_cog_teeth.union(tooth_3d)
 
-big_cog = big_cog_base.union(big_cog_teeth)
+        return small_cog_base.union(small_cog_teeth)
 
-# Teeth for the small cog (rectangular, large 3D fillets)
-small_cog_teeth = cq.Workplane("XY")
+    def _make_big_cog_tooth(
+        self,
+        radial_thickness: float,
+        base_width: float,
+        tip_width: float,
+        tooth_height: float,
+        fillet_3d: float
+    ) -> cq.Workplane:
+        """
+        Creates a trapezoidal tooth shape in 2D, extrudes it, then fillets
+        the vertical edges. The trapezoid coordinates are defined by base_width,
+        tip_width, and radial_thickness.
+        """
+        shape_2d = (
+            cq.Workplane("XY")
+            .polyline([
+                (0, -base_width / 2.0),
+                (0,  base_width / 2.0),
+                (radial_thickness,  tip_width / 2.0),
+                (radial_thickness, -tip_width / 2.0)
+            ])
+            .close()
+        )
+        tooth_3d = shape_2d.extrude(tooth_height)
+        return tooth_3d.edges("|Z").fillet(fillet_3d)
 
-for i in range(num_teeth_small):
-    angle_deg = i * (360.0 / num_teeth_small)
-    angle_rad = math.radians(angle_deg)
-
-    tooth_3d = make_small_cog_tooth(
-        radial_thickness = small_cog_radial_thick,  # ~1.5
-        tooth_width      = 1.0,
-        tooth_height     = small_cog_height,        # 1.5
-        fillet_3d        = 0.4
-    )
-
-    tooth_3d = (
-        tooth_3d
-        .rotate((0,0,0), (0,0,1), angle_deg)
-        .translate((
-            small_cog_base_radius * math.cos(angle_rad),
-            small_cog_base_radius * math.sin(angle_rad),
-            small_cog_z_start
-        ))
-    )
-    small_cog_teeth = small_cog_teeth.union(tooth_3d)
-
-small_cog = small_cog_base.union(small_cog_teeth)
-
-# Small top circle
-top_circle = (
-    cq.Workplane("XY")
-    .workplane(offset=top_circle_z_start)
-    .circle(top_circle_diam / 2.0)
-    .extrude(top_circle_height)
-)
-
-# Combine entire assembly
-full_assembly = (
-    wheel_with_pins_and_base
-    .union(lower_circle)
-    .union(big_cog)
-    .union(small_cog)
-    .union(top_circle)
-)
-
-# Export
-full_assembly.val().exportStl("cassette.stl", tolerance=0.01)
-print("Exported 'cassette.stl'.")
+    def _make_small_cog_tooth(
+        self,
+        radial_thickness: float,
+        tooth_width: float,
+        tooth_height: float,
+        fillet_3d: float
+    ) -> cq.Workplane:
+        """
+        Creates a rectangular tooth in 2D, extrudes it, then fillets all edges.
+        """
+        shape_2d = cq.Workplane("XY").rect(radial_thickness, tooth_width, centered=(False, True))
+        tooth_3d = shape_2d.extrude(tooth_height)
+        return tooth_3d.edges().fillet(fillet_3d)
